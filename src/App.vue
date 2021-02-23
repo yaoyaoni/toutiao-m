81720261574f673e19a189877b7895240063a6b6
yao-smile <1369880070@qq.com>
2021-02-23 16:55:31 +0800

    项目初始化  引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,23 @@
     <!-- 路由的出口 -->
     <router-view />
     <h1>123</h1>
+    <div>
+      <i class="toutiao toutiao-dianzan2"></i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
   </div>
 </template>
 <script>
-export default {
-
-};
+export default {};
 </script>
 
 <style lang="less">
